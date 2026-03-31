e17929d76429c165cb2b4aa3d59d8d880e119541
Arne Lammers <arnegoldeneagle@gmail.com>
2026-03-31 11:48:59 +0200

    use Path instead of os

--- workflow/scripts/convert_to_mzml.py
+++ workflow/scripts/convert_to_mzml.py
@@ -1,37 +1,33 @@
-import os
 import subprocess
 import argparse
 from glob import glob
+from pathlib import Path
 
+# Location of ThermoRawFileParser DLL
 THERMO_PARSER_DLL = "/home/mambauser/tools/ThermoRawFileParser-v.2.0.0-dev-linux/ThermoRawFileParser.dll"
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--dataset-dir", required=True, help="Path to dataset folder")
-    args = parser.parse_args()
+# Get snakemake parameters
+dataset_dir = Path(snakemake.input["dataset_dir"])
 
-    mzml_dir = os.path.join(args.dataset_dir, "mzml")
-    raw_dir = os.path.join(args.dataset_dir, "raw")
+mzml_dir = dataset_dir / "mzml"
+raw_dir = dataset_dir / "raw"
 
-    # If mzml folder exists, skip conversion
-    if os.path.exists(mzml_dir):
-        print(f"mzML folder already exists in {args.dataset_dir}, skipping conversion")
-        return
-
-    # Otherwise, convert all raw files
-    raw_files = glob(os.path.join(raw_dir, "*.raw"))
+# If mzml folder exists, skip conversion
+if mzml_dir.exists():
+    print(f"mzML folder already exists in {args.dataset_dir}, skipping conversion")
+    return
     
-    # Create mzml directory
-    os.makedirs(mzml_dir, exist_ok=True)
+# Otherwise, convert all raw files
+raw_files = glob(raw_dir / "*.raw")
 
-    for raw_file in raw_files:
-        print(f"Converting {raw_file} → {mzml_dir}")
-        subprocess.run([
-            "dotnet", THERMO_PARSER_DLL,
-            "-i", raw_file,
-            "-o", mzml_dir,
-            "-f", "1", "-p", "-g"
-        ], check=True)
+# Create mzml directory if not existing
+mzml_dir.mkdir(exist_ok=True)
 
-if __name__ == "__main__":
-    main()
+for raw_file in raw_files:
+    print(f"Converting {raw_file} → {mzml_dir}")
+    subprocess.run([
+        "dotnet", THERMO_PARSER_DLL,
+        "-i", raw_file,
+        "-o", mzml_dir,
+        "-f", "1", "-p", "-g"
+    ], check=True)
