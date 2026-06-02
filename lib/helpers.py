d27c03103f19d78c1efc9fb5dbbe9feec5ed4b44
Arne Lammers <arnegoldeneagle@gmail.com>
2026-06-02 10:17:39 +0000

    formatted files

--- lib/helpers.py
+++ lib/helpers.py
@@ -1,28 +1,22 @@
 from pathlib import Path
 
+
 def get_mzmine_params(config, wildcards):
     """Returns the mzmine parameters for the given dataset."""
     return config["datasets"][wildcards.dataset]["mzmine"]
 
+
 def get_samples(dataset):
     """Dynamically collects sample names ."""
-    raw_samples = {
-        p.stem
-        for p in Path(f"data/{dataset}/raw").glob("*.raw")
-    }
+    raw_samples = {p.stem for p in Path(f"data/{dataset}/raw").glob("*.raw")}
 
-    mzml_samples = {
-        p.stem
-        for p in Path(f"data/{dataset}/mzml_raw").glob("*.mzML.gz")
-    }
+    mzml_samples = {p.stem for p in Path(f"data/{dataset}/mzml_raw").glob("*.mzML.gz")}
 
     return sorted(raw_samples | mzml_samples)
+
 
 def get_spectral_library_files(config, dataset):
     """Returns local paths to spectral library files required by a dataset."""
     libs = config["datasets"][dataset]["mzmine"]["spectral_library_files"]
 
-    return [
-        f"resources/spectral_libraries/{lib}"
-        for lib in libs
-    ]
+    return [f"resources/spectral_libraries/{lib}" for lib in libs]
--- scripts/generate_mzmine_config.py
+++ scripts/generate_mzmine_config.py
@@ -1,12 +1,14 @@
 from pathlib import Path
 
-snakemake = snakemake  # type: ignore
+from snakemake.script import snakemake
 
 # Get snakemake parameters
 output_mzbatch = Path(snakemake.output["mzbatch"])
 
 # Get template
-template_file = Path("resources/templates/" + snakemake.params.settings["template"] + ".mzbatch")
+template_file = Path(
+    "resources/templates/" + snakemake.params.settings["template"] + ".mzbatch"
+)
 
 # Read template
 xml = template_file.read_text()
@@ -18,26 +20,50 @@ input_files = "\n\t\t\t".join(
 xml = xml.replace("{input_files}", input_files)
 
 # 2. Put metadata file in batch file
-xml = xml.replace("{metadata_file}", str(Path(snakemake.input["metadata_file"]).resolve()))
+xml = xml.replace(
+    "{metadata_file}", str(Path(snakemake.input["metadata_file"]).resolve())
+)
 
 # 3. Put output files in batch file
-output_dir = output_mzbatch.resolve().parent 
+output_dir = output_mzbatch.resolve().parent
 xml = xml.replace("{output_feature_table}", str(output_dir / "feature_table.csv"))
-xml = xml.replace("{output_feature_table_before_subtraction}", str(output_dir / "feature_table_before_subtraction.csv"))
+xml = xml.replace(
+    "{output_feature_table_before_subtraction}",
+    str(output_dir / "feature_table_before_subtraction.csv"),
+)
 xml = xml.replace("{output_annotations}", str(output_dir / "annotations.csv"))
 xml = xml.replace("{output_export_sirius}", str(output_dir / "export_sirius.mgf"))
 
 # 4. Put other parameters in batch file
-xml = xml.replace("{rt_range_min}", str(float(snakemake.params.settings["retention_time_range"][0])))
-xml = xml.replace("{rt_range_max}", str(float(snakemake.params.settings["retention_time_range"][1])))
-xml = xml.replace("{minimum_feature_height}", str(float(snakemake.params.settings["minimum_feature_height"])))
-xml = xml.replace("{approximate_feature_fwhm}", str(float(snakemake.params.settings["approximate_feature_fwhm"])))
-xml = xml.replace("{blank_subtraction_min_blank_presence}", str(float(snakemake.params.settings["blank_subtraction"]["min_blank_presence"])))
-xml = xml.replace("{blank_subtraction_fold_change_threshold}", str(float(snakemake.params.settings["blank_subtraction"]["fold_change_threshold"])))
+xml = xml.replace(
+    "{rt_range_min}", str(float(snakemake.params.settings["retention_time_range"][0]))
+)
+xml = xml.replace(
+    "{rt_range_max}", str(float(snakemake.params.settings["retention_time_range"][1]))
+)
+xml = xml.replace(
+    "{minimum_feature_height}",
+    str(float(snakemake.params.settings["minimum_feature_height"])),
+)
+xml = xml.replace(
+    "{approximate_feature_fwhm}",
+    str(float(snakemake.params.settings["approximate_feature_fwhm"])),
+)
+xml = xml.replace(
+    "{blank_subtraction_min_blank_presence}",
+    str(float(snakemake.params.settings["blank_subtraction"]["min_blank_presence"])),
+)
+xml = xml.replace(
+    "{blank_subtraction_fold_change_threshold}",
+    str(float(snakemake.params.settings["blank_subtraction"]["fold_change_threshold"])),
+)
 
 # 5. Put spectral library files in batch file
 spectral_library_files = "\n\t\t\t".join(
-    [f"<file>{str(Path('resources/spectral_libraries/' + f).resolve())}</file>" for f in snakemake.params.settings["spectral_library_files"]]
+    [
+        f"<file>{str(Path('resources/spectral_libraries/' + f).resolve())}</file>"
+        for f in snakemake.params.settings["spectral_library_files"]
+    ]
 )
 xml = xml.replace("{spectral_library_files}", spectral_library_files)
 
